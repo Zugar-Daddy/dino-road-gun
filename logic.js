e9741f651aaf7be75ba5c0c743965310df78a750
Madhav Bhardwaj <69211934+Zugar-Daddy@users.noreply.github.com>
2026-07-13 23:55:24 +0530

    Working game wit okayish style

--- logic.js
+++ logic.js
@@ -1,1 +1,80 @@
 console.log("hello worlds");
+
+let humanScore = 0;
+let computerScore = 0;
+// 1-> Dino, 2-> Road, 3-> Gun
+
+// 1 vs 2 -> 1 wins
+// 2 vs 3 -> 2 wins
+// 3 vs 1 -> 3 wins
+function getComputerChoice(){
+    let num = Math.random();
+
+    if(num < 0.33){
+        return 1;
+    }
+    else if(num < 0.66){
+        return 2;
+    }
+    else{
+        return 3;
+    }
+}
+
+// 0-> pc, 1-> human, 2-> draw
+function giveResult(val){
+    const doc = document.getElementsByClassName("result-text")[0];
+    if(val == 0){
+        doc.innerHTML = "OOFF, PC WON :(";
+    }
+    else if(val == 1){
+        doc.innerHTML = "YAYYY, You won :D";
+    }
+    else if(val == 2){
+        doc.innerHTML = "DRAWWWW :/";
+    }
+}
+
+function updateWinner(humanwin){
+    if(humanwin){
+        document.getElementsByClassName("score-box-human")[0].innerHTML = `Human Score: ${humanScore}`;
+        giveResult(1);
+    }
+    else{
+        document.getElementsByClassName("score-box-computer")[0].innerHTML = `Computer Score: ${computerScore}`;
+        giveResult(0);
+
+    }
+}
+
+function GameResult(human){
+    let pc = getComputerChoice();
+    let hwin = false;
+    if(human == pc){
+        giveResult(2);
+        return;
+    }
+    else if(human == 1 && pc == 2){
+        humanScore++;
+        hwin = true;
+    }
+    else if(human == 2 && pc == 3){
+        humanScore++;
+        hwin = true;
+    }
+    else if(human == 3 && pc == 1){
+        humanScore++;
+        hwin = true;
+    }
+    else if(pc == 1 && human == 2){
+        computerScore++;
+    }
+    else if(pc == 2 && human == 3){
+        computerScore++;
+    }
+    else{
+        computerScore++;
+    } 
+
+    updateWinner(hwin);
+}
